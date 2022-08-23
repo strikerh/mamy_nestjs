f06ac7792f7c2f7d8888bb5922d259d979fb24d0
Hazem Safwat <hazem.xmotion@gmail.com>
2022-08-23 22:43:17 +0300

    config production config

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(8080);
+  await app.listen(112808);
 }
 
 bootstrap();
+
+
